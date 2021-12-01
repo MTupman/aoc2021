56db5ae0ea9268c4b4f5d2805e01c0ab489c1a1f
Matt Tupman <matt@tupman.eu>
2021-12-01 19:05:49 +0000

    day 1_task 1 - 1 error issue

---
+++ Python_Sandbox/AoC_2021/Day01/day01.py
@@ -0,0 +1,23 @@
+f = open("C:\\Users\\mtupm\\Documents\\VSCode\\Python_Sandbox\\AoC_2021\\Day01\\input.txt","r")
+# f = open("C:\\Users\\mtupm\\Documents\\VSCode\\Python_Sandbox\\AoC_2021\\Day01\\input_test.txt","r")
+inputLines = f.readlines()
+newLines = []
+
+for line in inputLines:
+    newLines.append(line.strip())
+
+output = 0
+prevDepth = newLines[0]
+
+# for index,depth in enumerate(newLines):
+#     currDepth = newLines[index]
+#     if currDepth > prevDepth:
+#         output += 1    
+#     prevDepth = currDepth
+
+nextSteps = newLines[1:]        #from item 1 to end
+originalSteps = newLines[:-1]   #from item 0 to -1 from end
+isIncrease = [n >= o for n,o in zip(nextSteps, originalSteps)]
+output = sum(isIncrease)
+
+print(output)
